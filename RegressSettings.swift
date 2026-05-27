a36637b079b97cb7df2b9a6137dcb3ceff0b0422
Ponchik0 <ponchik0@regtel.com>
2026-05-27 22:04:28 +0300

    Restore ldid for compilation in workflow and fix selector warning

--- RegressSettings.swift
+++ RegressSettings.swift
@@ -390,7 +390,7 @@ import UIKit
         let supportsKey = "supportsAlternateAppIcons"
         guard let supports = UIApplication.shared.value(forKey: supportsKey) as? Bool, supports else { return }
         let apiName = (iconName == "Regress Default") ? nil : iconName.replacingOccurrences(of: " ", with: "_").lowercased()
-        UIApplication.shared.perform(Selector(("setAlternateIconName:completionHandler:")), with: apiName, with: nil)
+        UIApplication.shared.perform(NSSelectorFromString("setAlternateIconName:completionHandler:"), with: apiName, with: nil)
     }
     
     private func presentBadgePicker() {
